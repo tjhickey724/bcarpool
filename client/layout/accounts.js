e12aa63acb0f6c3fb79265ecc67dfb5b1ce680f9
zhongqil <zhongqilpostoffice@gmail.com>
2015-12-19 17:56:13 -0500

    added phone validation

--- client/layout/accounts.js
+++ client/layout/accounts.js
@@ -40,7 +40,7 @@ AccountsTemplates.addFields([
       displayName: "Brandeis Email",
       //re: /.+@(.+){2,}\.(.+){2,}/,
       re: /.+@brandeis\.edu/,
-      errStr: 'Invalid email',
+      errStr: 'Invalid Email',
   },
   pwd
 ]);
@@ -78,70 +78,10 @@ AccountsTemplates.addField({
 });
 
 AccountsTemplates.addField({
-    _id: "phone",
+    _id: "XXX-XXX-XXXX",
     type: "text",
     displayName: "Phone",
+    re: /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+    errStr: 'Invalid Phone Number',
     required: true,
 });
-
-/*
-
-Accounts.ui.config({
-    requestPermissions: {},
-    extraSignupFields: [{
-        fieldName: 'firstName',
-        fieldLabel: 'First name',
-        inputType: 'text',
-        visible: true,
-        validate: function(value, errorFunction) {
-          if (!value) {
-            errorFunction("Please write your first name");
-            return false;
-          } else {
-            return true;
-          }
-        }
-    }, {
-        fieldName: 'lastName',
-        fieldLabel: 'Last name',
-        inputType: 'text',
-        visible: true,
-    }, {
-        fieldName: 'gender',
-        showFieldLabel: false,      // If true, fieldLabel will be shown before radio group
-        fieldLabel: 'Gender',
-        inputType: 'radio',
-        radioLayout: 'vertical',    // It can be 'inline' or 'vertical'
-        data: [{                    // Array of radio options, all properties are required
-            id: 1,                  // id suffix of the radio element
-            label: 'Male',          // label for the radio element
-            value: 'm'              // value of the radio element, this will be saved.
-          }, {
-            id: 2,
-            label: 'Female',
-            value: 'f',
-            checked: 'checked'
-        }],
-        visible: true
-    }, {
-        fieldName: 'phone',
-        fieldLabel: 'Phone Number',
-        inputType: 'text',
-        visible: true,
-    }, {
-        fieldName: 'terms',
-        fieldLabel: 'I accept the terms and conditions',
-        inputType: 'checkbox',
-        visible: true,
-        saveToProfile: false,
-        validate: function(value, errorFunction) {
-            if (value) {
-                return true;
-            } else {
-                errorFunction('You must accept the terms and conditions.');
-                return false;
-            }
-        }
-    }]
-});
-*/
